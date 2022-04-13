5286bd4b0028bd6122e62f527be65118b151e378
Darek Vassalli <darek.vassalli@studentfr.ch>
2022-04-13 15:06:35 +0200

    fin du menu burger et début du css main

--- index.js
+++ index.js
@@ -1,41 +1,3 @@
-var controller = new ScrollMagic.Controller();
-
-var btween = new TweenMax.to(".image-b", 2, {
-  y: -100,
-});
-
-var containerScene = new ScrollMagic.Scene({
-  offset: 500,
-  duration: 400,
-})
-  .setTween(btween)
-  .addIndicators()
-  .addTo(controller);
-
-var otween = new TweenMax.to(".image-o", 1.5, {
-  y: 300,
-});
-
-var containerScene = new ScrollMagic.Scene({
-  offset: 180,
-  duration: 800,
-})
-  .setTween(otween)
-  .addIndicators()
-  .addTo(controller);
-
-var mtween = new TweenMax.to(".image-m", 3, {
-  y: 500,
-});
-
-var containerScene = new ScrollMagic.Scene({
-  offset: 50,
-  duration: 800,
-})
-  .setTween(mtween)
-  .addIndicators()
-  .addTo(controller);
-
 // burger
 
 const burger = document.querySelector('.burger');
@@ -49,6 +11,42 @@ burger.addEventListener('click', () => {
 navi.addEventListener('click', () => {
     navi.classList.toggle('navi-activ');
 });
+
+
+var controller = new ScrollMagic.Controller();
+
+var btween = new TweenMax.to(".image-b", 2, {
+  y: -100,
+});
+
+var containerScene = new ScrollMagic.Scene({
+  offset: 500,
+  duration: 400,
+})
+  .setTween(btween)
+  .addTo(controller);
+
+var otween = new TweenMax.to(".image-o", 1.5, {
+  y: 300,
+});
+
+var containerScene = new ScrollMagic.Scene({
+  offset: 180,
+  duration: 800,
+})
+  .setTween(otween)
+  .addTo(controller);
+
+var mtween = new TweenMax.to(".image-m", 3, {
+  y: 500,
+});
+
+var containerScene = new ScrollMagic.Scene({
+  offset: 50,
+  duration: 800,
+})
+  .setTween(mtween)
+  .addTo(controller);
 
 // carousel
 
